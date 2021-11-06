db1cab2691816efe099245011484ac7a0b66b15b
DanSoW <67583913+DanSoW@users.noreply.github.com>
2021-11-06 20:17:05 +0800

    Добавьте файлы проекта.

---
+++ Window.Designer.cs
@@ -0,0 +1,135 @@
+﻿
+namespace MSOffice
+{
+	partial class Window
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button4 = new System.Windows.Forms.Button();
+			this.button5 = new System.Windows.Forms.Button();
+			this.button6 = new System.Windows.Forms.Button();
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Location = new System.Drawing.Point(12, 176);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(544, 493);
+			this.pictureBox1.TabIndex = 2;
+			this.pictureBox1.TabStop = false;
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(176, 12);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(189, 42);
+			this.button3.TabIndex = 3;
+			this.button3.Text = "Загрузить изображение";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
+			// 
+			// button4
+			// 
+			this.button4.Location = new System.Drawing.Point(176, 60);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(189, 42);
+			this.button4.TabIndex = 4;
+			this.button4.Text = "Добавить изображение в Word";
+			this.button4.UseVisualStyleBackColor = true;
+			this.button4.Click += new System.EventHandler(this.button4_Click);
+			// 
+			// button5
+			// 
+			this.button5.Location = new System.Drawing.Point(822, 60);
+			this.button5.Name = "button5";
+			this.button5.Size = new System.Drawing.Size(189, 42);
+			this.button5.TabIndex = 6;
+			this.button5.Text = "Сохранить изменения";
+			this.button5.UseVisualStyleBackColor = true;
+			this.button5.Click += new System.EventHandler(this.button5_Click);
+			// 
+			// button6
+			// 
+			this.button6.Location = new System.Drawing.Point(822, 12);
+			this.button6.Name = "button6";
+			this.button6.Size = new System.Drawing.Size(189, 42);
+			this.button6.TabIndex = 5;
+			this.button6.Text = "Открыть Excel";
+			this.button6.UseVisualStyleBackColor = true;
+			this.button6.Click += new System.EventHandler(this.button6_Click);
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.Location = new System.Drawing.Point(619, 176);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.RowHeadersWidth = 51;
+			this.dataGridView1.RowTemplate.Height = 24;
+			this.dataGridView1.Size = new System.Drawing.Size(577, 493);
+			this.dataGridView1.TabIndex = 7;
+			this.dataGridView1.EditingControlShowing += new System.Windows.Forms.DataGridViewEditingControlShowingEventHandler(this.dataGridView1_EditingControlShowing);
+			// 
+			// openFileDialog1
+			// 
+			this.openFileDialog1.FileName = "openFileDialog1";
+			// 
+			// Window
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1208, 681);
+			this.Controls.Add(this.dataGridView1);
+			this.Controls.Add(this.button5);
+			this.Controls.Add(this.button6);
+			this.Controls.Add(this.button4);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.pictureBox1);
+			this.Name = "Window";
+			this.Text = "Window";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.Button button5;
+		private System.Windows.Forms.Button button6;
+		private System.Windows.Forms.DataGridView dataGridView1;
+		private System.Windows.Forms.OpenFileDialog openFileDialog1;
+	}
+}
+
---
+++ Window.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Xceed.Document.NET;
+using Xceed.Words.NET;
+using Microsoft.Office.Interop.Excel;
+using System.Runtime.InteropServices;
+
+namespace MSOffice
+{
+	public partial class Window : Form
+	{
+		private string _lastOpenExcelFile = "";
+
+		public Window()
+		{
+			InitializeComponent();
+		}
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+			openFileDialog1.Filter = "Файлы изображений|*.bmp;*.png;*.jpg";
+			if (openFileDialog1.ShowDialog() != DialogResult.OK)
+				return;
+
+			try
+			{
+				pictureBox1.Image = System.Drawing.Image.FromFile(openFileDialog1.FileName);
+				pictureBox1.ImageLocation = openFileDialog1.FileName;
+			}
+			catch (OutOfMemoryException)
+			{
+				MessageBox.Show("Ошибка чтения картинки");
+				return;
+			}
+
+			pictureBox1.Invalidate();
+		}
+
+		private void button4_Click(object sender, EventArgs e)
+		{
+			openFileDialog1.Filter = "Файлы Word|*.docx";
+			if (openFileDialog1.ShowDialog() != DialogResult.OK)
+				return;
+
+			// Путь к документу
+			string pathDocument = openFileDialog1.FileName;
+
+			// Загрузка документа
+			DocX document = DocX.Load(pathDocument);
+
+			// загрузка изображения
+			Xceed.Document.NET.Image image = document.AddImage(pictureBox1.ImageLocation);
+
+			// Создание параграфа
+			Paragraph paragraph = document.InsertParagraph();
+
+			// Вставка изображения в параграф
+			paragraph.AppendPicture(image.CreatePicture());
+
+			// Выравнивание параграфа по центру
+			paragraph.Alignment = Alignment.center;
+
+			// Сохраняем документ
+			document.Save();
+
+			MessageBox.Show("Картинка добавлена в документ!");
+		}
+
+		private void button6_Click(object sender, EventArgs e)
+		{
+			System.Data.DataTable dt = new System.Data.DataTable();
+			DataRow row;
+
+			openFileDialog1.Filter = "Файлы Excel|*.xlsx";
+			if (openFileDialog1.ShowDialog() != DialogResult.OK)
+			{
+				return;
+			}
+
+			_lastOpenExcelFile = openFileDialog1.FileName;
+			try
+			{
+				Microsoft.Office.Interop.Excel.Application excelApp = new Microsoft.Office.Interop.Excel.Application();
+				Microsoft.Office.Interop.Excel.Workbook excelWorkbook = excelApp.Workbooks.Open(_lastOpenExcelFile);
+				Microsoft.Office.Interop.Excel._Worksheet excelWorksheet = excelWorkbook.Sheets[1];
+				Microsoft.Office.Interop.Excel.Range excelRange = excelWorksheet.UsedRange;
+
+				int rowCount = excelRange.Rows.Count;
+				int colCount = excelRange.Columns.Count;
+
+				for (int i = 1; i <= rowCount; i++)
+				{
+					for (int j = 1; j <= colCount; j++)
+					{
+						dt.Columns.Add(excelRange.Cells[i, j].Value2.ToString());
+					}
+					break;
+				}
+
+				int rowCounter = 0;
+				for (int i = 2; i <= rowCount; i++)
+				{
+					row = dt.NewRow();
+					rowCounter = 0;
+					for (int j = 1; j <= colCount; j++)
+					{
+						if (excelRange.Cells[i, j] != null && excelRange.Cells[i, j].Value2 != null)
+						{
+							row[rowCounter] = excelRange.Cells[i, j].Value2.ToString();
+						}
+						else
+						{
+							row[rowCounter] = "";
+						}
+						rowCounter++;
+					}
+					dt.Rows.Add(row);
+				}
+
+				dataGridView1.DataSource = dt;
+
+				// Закрытие и очистка Excel процесса
+				GC.Collect();
+				GC.WaitForPendingFinalizers();
+				Marshal.ReleaseComObject(excelRange);
+				Marshal.ReleaseComObject(excelWorksheet);
+				// Выход из Excel
+				excelWorkbook.Close();
+				Marshal.ReleaseComObject(excelWorkbook);
+				excelApp.Quit();
+				Marshal.ReleaseComObject(excelApp);
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+				_lastOpenExcelFile = "";
+			}
+		}
+
+		private void button5_Click(object sender, EventArgs e)
+		{
+			if(_lastOpenExcelFile.Length <= 0)
+			{
+				MessageBox.Show("Необходимо открытие документа Excel");
+				return;
+			}
+
+			try
+			{
+				Microsoft.Office.Interop.Excel.Application excelApp = new Microsoft.Office.Interop.Excel.Application();
+				Microsoft.Office.Interop.Excel.Workbook excelWorkbook = excelApp.Workbooks.Open(_lastOpenExcelFile);
+				Microsoft.Office.Interop.Excel._Worksheet excelWorksheet = excelWorkbook.Sheets[1];
+				Microsoft.Office.Interop.Excel.Range excelRange = excelWorksheet.UsedRange;
+
+				for (int i = 1; i < dataGridView1.Columns.Count + 1; i++)
+				{
+					excelWorksheet.Cells[1, i] = dataGridView1.Columns[i - 1].HeaderText;
+				}
+
+				for (int i = 0; i < dataGridView1.Rows.Count - 1; i++)
+				{
+					for (int j = 0; j < dataGridView1.Columns.Count; j++)
+					{
+						excelWorksheet.Cells[i + 2, j + 1] = dataGridView1.Rows[i].Cells[j].Value.ToString();
+					}
+				}
+
+
+				excelWorkbook.SaveAs(_lastOpenExcelFile,
+					Type.Missing, Type.Missing, Type.Missing, Type.Missing, 
+					Type.Missing, Microsoft.Office.Interop.Excel.XlSaveAsAccessMode.xlExclusive, 
+					Type.Missing, Type.Missing, Type.Missing, Type.Missing);
+
+				// Закрытие и очистка Excel процесса
+				GC.Collect();
+				GC.WaitForPendingFinalizers();
+				Marshal.ReleaseComObject(excelRange);
+				Marshal.ReleaseComObject(excelWorksheet);
+				// Выход из Excel
+				excelWorkbook.Close();
+				Marshal.ReleaseComObject(excelWorkbook);
+				excelApp.Quit();
+				Marshal.ReleaseComObject(excelApp);
+
+				MessageBox.Show("Данные были сохранены!");
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+				_lastOpenExcelFile = "";
+			}
+		}
+
+		private void dataGridView1_EditingControlShowing(object sender, DataGridViewEditingControlShowingEventArgs e)
+		{
+			System.Windows.Forms.TextBox tb = (System.Windows.Forms.TextBox)e.Control;
+			tb.KeyPress += new KeyPressEventHandler(tb_KeyPress);
+		}
+
+		void tb_KeyPress(object sender, KeyPressEventArgs e)
+		{
+
+			if (!(Char.IsDigit(e.KeyChar)))
+			{
+				if (e.KeyChar != (char)Keys.Back)
+				{
+					e.Handled = true;
+				}
+			}
+		}
+	}
+}
